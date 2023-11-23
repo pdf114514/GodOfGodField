6b39a135402042079937bd5966a974eb3a5dc51a
pdf114514 <haruton321@outlook.jp>
2023-11-24 00:55:58 +0900

    FIX

--- Shared/JsonElementExtension.cs
+++ Shared/JsonElementExtension.cs
@@ -3,7 +3,7 @@ using System.Text.Json;
 namespace GodOfGodField.Shared;
 
 public static class JsonElementExtension {
-    public static string GetStringValue(this JsonElement element) => element.GetStringValue();
+    public static string GetStringValue(this JsonElement element) => element.GetProperty("stringValue").GetString()!;
     public static int GetIntValue(this JsonElement element) => int.Parse(element.GetProperty("integerValue").GetString()!);
     public static JsonElement GetMapValue(this JsonElement element) => element.GetProperty("mapValue");
     public static JsonElement GetFieldsValue(this JsonElement element) => element.GetProperty("fields");
